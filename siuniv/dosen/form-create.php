d9605ab289532624f81e161227bad08c82c7d882
Lazua <103889259+Lazua@users.noreply.github.com>
2022-09-14 20:08:29 +0700

    Add files via upload

---
+++ siuniv/dosen/form-create.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tambah data dosen</h1>
+    <form action="create.php" method="POST">
+        Mama dosen: <input type="text" name="nama_dosen">
+        <br>
+        No.telp: <input type="text" name="telp">
+        <br>
+        <input type="submit" value="Simpan" name="btnsimpan">
+    </form>
+</body>
+</html>
